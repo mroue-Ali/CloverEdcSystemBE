c463ade79494f246d0a25695d64fbc2da48b12b2
MohammadChalhoub <mohamed.chalhoub2001@gmail.com>
2025-02-04 17:38:08 +0200

    ..

---
+++ CloverEdc.UnitTests/ServicesTest.cs
@@ -0,0 +1,16 @@
+
+
+using CloverEdc.Business.Interfaces;
+
+namespace CloverEdc.UnitTests;
+
+public class ServicesTest
+{
+    private readonly IPiService _piService;
+    [Fact]
+    public void Test1()
+    {
+        //arrange
+        
+    }
+}
